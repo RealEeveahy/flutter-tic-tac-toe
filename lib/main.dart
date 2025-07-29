268573d5f1ec89ee559a13686f4ef73c43eeca22
RealEeveahy <mason.mclean@hotmail.com>
2025-07-29 14:01:28 +0800

    Simplify Initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tic-Tac-Toe'),
+        ),
+
+        // The body of the screen appears below the AppBar.
+        // Since there's no Center widget wrapping this,
+        // the text will be aligned to the top-left by default.
+        body: Text(
+          'Dice and Dungeons!',
+          // TextStyle allows us to change how the text looks.
+          // Here we set the font size to 24.
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    ),
+  );
 }
